fedf386c16d0874a6642d6f232d262ba6ef0c5a9
Andrew Dryga <andrew@dryga.com>
2016-08-24 00:21:11 +0300

    Code style

--- test/unit/jvalid_test.exs
+++ test/unit/jvalid_test.exs
@@ -7,15 +7,15 @@ defmodule SampleUsageTest do
   use_schema :schema, "test/support/schema.json"
 
   test "validates used schema" do
-    assert true == valid_schema?(:schema, %{"foo" => "bar"})
-    assert false == valid_schema?(:schema, %{"foo" => 1})
+    assert valid_schema?(:schema, %{"foo" => "bar"})
+    refute valid_schema?(:schema, %{"foo" => 1})
   end
 
   test "validates inline schema" do
     schema = "test/support/schema.json"
     |> load_schema
 
-    assert true == valid_schema?(schema, %{"foo" => "bar"})
-    assert false == valid_schema?(schema, %{"foo" => 1})
+    assert valid_schema?(schema, %{"foo" => "bar"})
+    refute valid_schema?(schema, %{"foo" => 1})
   end
 end
